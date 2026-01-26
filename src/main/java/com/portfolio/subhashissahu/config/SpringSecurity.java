67ce7cfcc078d431d460e6f702f37471aa6d5aa9
Subhashis Sahu <sahusubhashis01@gmail.com>
2026-01-26 22:20:24 +0530

    some application properties change

--- src/main/java/com/portfolio/subhashissahu/config/SpringSecurity.java
+++ src/main/java/com/portfolio/subhashissahu/config/SpringSecurity.java
@@ -38,7 +38,7 @@ public class SpringSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","http://subhashis-sahu.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
