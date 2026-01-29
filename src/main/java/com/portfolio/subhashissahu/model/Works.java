d1b54447a3c457c743568ae09fa0d21160a670a1
Subhashis Sahu <sahusubhashis01@gmail.com>
2026-01-29 23:34:24 +0530

    update

--- src/main/java/com/portfolio/subhashissahu/model/Works.java
+++ src/main/java/com/portfolio/subhashissahu/model/Works.java
@@ -8,16 +8,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 public class Works {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String tittle;
     private String describtion;
     private String techStack;
