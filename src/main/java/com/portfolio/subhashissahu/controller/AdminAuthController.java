764b44f49a1d6ff9ddf9b45b048a506b1779d5af
Subhashis Sahu <sahusubhashis01@gmail.com>
2026-01-30 02:20:48 +0530

    add security

--- src/main/java/com/portfolio/subhashissahu/controller/AdminAuthController.java
+++ src/main/java/com/portfolio/subhashissahu/controller/AdminAuthController.java
@@ -2,6 +2,7 @@ package com.portfolio.subhashissahu.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +23,18 @@ public class AdminAuthController {
         this.jwtUtil = jwtUtil;
     }
 
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest req) {
 
         // HARD-CODED admin (for now)
-        if ("admin".equals(req.getUserName()) &&
-            "admin123".equals(req.getPassword())) {
+        if (adminUsername.equals(req.getUserName()) &&
+            adminPassword.equals(req.getPassword())) {
 
             String token = jwtUtil.generateToken("admin");
             return ResponseEntity.ok(Map.of("token", token));
